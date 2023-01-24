ee3bbace79adb1c0466d14964a1cfe40ec80ab29
Nikhil <nikhilshah.net@hotmail.com>
2023-01-24 13:24:29 +0530

    WEbAPI call

---
+++ day 5/MVC_API_calls/MVC_API_calls/Controllers/ExternalDataController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_API_calls.Controllers
+{
+    public class ExternalDataController : Controller
+    {
+    
+        public IActionResult PostData()
+        {
+            return View();
+        }
+
+        public IActionResult PhotosData()
+        {
+            return View();
+        }
+    }
+}
